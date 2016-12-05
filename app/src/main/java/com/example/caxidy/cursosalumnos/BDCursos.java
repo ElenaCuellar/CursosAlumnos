43f85c7ab3167499c72780d836edb1b872d971e8
ElenaCuellar <caxidy.x3@gmail.com>
2016-12-05 23:21:16 +0100

    Some changes

--- app/src/main/java/com/example/caxidy/cursosalumnos/BDCursos.java
+++ app/src/main/java/com/example/caxidy/cursosalumnos/BDCursos.java
@@ -1,8 +1,12 @@
 package com.example.caxidy.cursosalumnos;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
 
 public class BDCursos extends SQLiteOpenHelper {
     private static Curso curso;
@@ -45,5 +49,169 @@ public class BDCursos extends SQLiteOpenHelper {
         db.execSQL("DROP TABLE IF EXISTS "+NOMBRE_TABLA_ASIGNATURAS);
 
         onCreate(db);
+    }
+
+    //SELECT:
+
+    public boolean hayCursos(){
+        SQLiteDatabase db = getReadableDatabase();
+        if (db != null) {
+            String[] campos = {"COUNT(id)"};
+            Cursor c = db.query("Cursos", campos,null, null, null, null, null, null);
+            if (c.moveToFirst())
+                if(c.getInt(0)>0)
+                    return true;
+                else
+                    return false;
+            c.close();
+        }
+        db.close();
+        return false;
+    }
+
+    public boolean hayAlumnos(){
+        SQLiteDatabase db = getReadableDatabase();
+        if (db != null) {
+            String[] campos = {"COUNT(id)"};
+            Cursor c = db.query("Alumnos", campos,null, null, null, null, null, null);
+            if (c.moveToFirst())
+                if(c.getInt(0)>0)
+                    return true;
+                else
+                    return false;
+            c.close();
+        }
+        db.close();
+        return false;
+    }
+
+    public boolean hayProfesores(){
+        SQLiteDatabase db = getReadableDatabase();
+        if (db != null) {
+            String[] campos = {"COUNT(id)"};
+            Cursor c = db.query("Profesores", campos,null, null, null, null, null, null);
+            if (c.moveToFirst())
+                if(c.getInt(0)>0)
+                    return true;
+                else
+                    return false;
+            c.close();
+        }
+        db.close();
+        return false;
+    }
+
+    public boolean hayAsignaturas(){
+        SQLiteDatabase db = getReadableDatabase();
+        if (db != null) {
+            String[] campos = {"COUNT(id)"};
+            Cursor c = db.query("Asignaturas", campos,null, null, null, null, null, null);
+            if (c.moveToFirst())
+                if(c.getInt(0)>0)
+                    return true;
+                else
+                    return false;
+            c.close();
+        }
+        db.close();
+        return false;
+    }
+
+    public ArrayList<Curso> listarCursos(){
+        ArrayList<Curso> arrayCursos = new ArrayList<>();
+        SQLiteDatabase db = getReadableDatabase();
+        if (db != null) {
+            String[] campos = {"id", "nombre"};
+            Cursor c = db.query("Cursos", campos,null, null, null, null, null, null);
+            if (c.moveToFirst())
+                do {
+                    curso = new Curso(c.getInt(0), c.getString(1));
+                    arrayCursos.add(curso);
+                }while(c.moveToNext());
+            c.close();
+        }
+        db.close();
+        if(arrayCursos.size()>0)
+            return arrayCursos;
+        else
+            return null;
+    }
+
+    public ArrayList<Alumno> listarAlumnos(int idCurso){
+        ArrayList<Alumno> arrayAlumnos = new ArrayList<>();
+        SQLiteDatabase db = getReadableDatabase();
+        if (db != null) {
+            String[] campos = {"id", "nombre", "apellidos", "idCurso"};
+            Cursor c = db.query("Alumnos", campos,"idCurso="+idCurso, null, null, null, null, null);
+            if (c.moveToFirst())
+                do {
+                    alumno = new Alumno(c.getInt(0), c.getString(1),c.getString(2),c.getInt(3));
+                    arrayAlumnos.add(alumno);
+                }while(c.moveToNext());
+            c.close();
+        }
+        db.close();
+        if(arrayAlumnos.size()>0)
+            return arrayAlumnos;
+        else
+            return null;
+    }
+
+    //INSERT
+
+    public long insertarCurso(Curso curso){
+        long numReg = -1;
+        SQLiteDatabase db = getWritableDatabase();
+        if (db != null) {
+            ContentValues valores = new ContentValues();
+            valores.put("id", curso.getID());
+            valores.put("nombre", curso.getNombre());
+            numReg = db.insert("Cursos", null, valores);
+        }
+        db.close();
+        return numReg;
+    }
+
+    public long insertarAlumno(Alumno alumno){
+        long numReg = -1;
+        SQLiteDatabase db = getWritableDatabase();
+        if (db != null) {
+            ContentValues valores = new ContentValues();
+            valores.put("id", alumno.getID());
+            valores.put("nombre", alumno.getNombre());
+            valores.put("apellidos",alumno.getApellido());
+            valores.put("idCurso",alumno.getIdCurso());
+            numReg = db.insert("Alumnos", null, valores);
+        }
+        db.close();
+        return numReg;
+    }
+
+    public long insertarProfesor(Profesor profesor){
+        long numReg = -1;
+        SQLiteDatabase db = getWritableDatabase();
+        if (db != null) {
+            ContentValues valores = new ContentValues();
+            valores.put("id", profesor.getID());
+            valores.put("nombre", profesor.getNombre());
+            numReg = db.insert("Profesores", null, valores);
+        }
+        db.close();
+        return numReg;
+    }
+
+    public long insertarAsignatura(Asignatura asignatura){
+        long numReg = -1;
+        SQLiteDatabase db = getWritableDatabase();
+        if (db != null) {
+            ContentValues valores = new ContentValues();
+            valores.put("id", asignatura.getID());
+            valores.put("nombre", asignatura.getNombre());
+            valores.put("idProfesor",asignatura.getIdProfesor());
+            valores.put("idCurso",asignatura.getIdCurso());
+            numReg = db.insert("Asignaturas", null, valores);
+        }
+        db.close();
+        return numReg;
     }
 }
--- app/src/main/java/com/example/caxidy/cursosalumnos/MainActivity.java
+++ app/src/main/java/com/example/caxidy/cursosalumnos/MainActivity.java
@@ -1,4 +1,4 @@
-/*-ExpandableListView con los cursos del ciclo y sus alumnos
+/*-Localizar todos los String
 -Navigation Drawer con ListView: en el drawer, cuando pulsas un item d asignatura sale un dialog en el que pone
 el curso en el que se da y el profesor que la imparte.*/
 
@@ -18,9 +18,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ExpandableListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    ExpandableListView listaExpandable;
+    ExpandableListAdapter adaptadorExpandable;
+    List<String> listaCabeceras;
+    HashMap<String,List<String>> listaHijos;
+    BDCursos bd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,14 +41,42 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        bd = new BDCursos(this);
+
+        //Obtener el ExpandableListView y llenarlo de datos
+        listaExpandable = (ExpandableListView) findViewById(R.id.listExpandable);
+        mostrarDatos();
+        //Concretar el adaptador
+        adaptadorExpandable = new ExpandableListAdapter(this,listaCabeceras,listaHijos);
+        listaExpandable.setAdapter(adaptadorExpandable);
+
+        //Listener de la lista Expandable (al pulsar en un hijo):
+        listaExpandable.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public boolean onChildClick(ExpandableListView parent, View v,
+                                        int groupPosition, int childPosition, long id) {
+                Toast.makeText(getApplicationContext(), "Alumno: " + listaHijos.get(listaCabeceras.get(groupPosition)).get(childPosition),
+                        Toast.LENGTH_SHORT).show();
+                return false;
             }
         });
+        //Listener para cuando se expande un grupo:
+        listaExpandable.setOnGroupExpandListener(new ExpandableListView.OnGroupExpandListener() {
+            @Override
+            public void onGroupExpand(int groupPosition) {
+                Toast.makeText(getApplicationContext(), "El grupo "+listaCabeceras.get(groupPosition) + " se ha abierto",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+        //Listener al contraer un grupo:
+        listaExpandable.setOnGroupCollapseListener(new ExpandableListView.OnGroupCollapseListener() {
+            @Override
+            public void onGroupCollapse(int groupPosition) {
+                Toast.makeText(getApplicationContext(), "El grupo "+listaCabeceras.get(groupPosition) + " se ha cerrado",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -46,6 +86,120 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    public void mostrarDatos(){
+        List<String> grupoAl;
+        ArrayList<Alumno> listaAl = new ArrayList<>();
+        listaCabeceras = new ArrayList<>();
+        listaHijos = new HashMap<String, List<String>>();
+
+        //Comprobar si las BD de Cursos o Alumnos estan vacias para añadir los cursos y alumnos necesarios...
+        if(!bd.hayCursos()){
+            llenarBDCursos();
+        }
+
+        if(!bd.hayAlumnos()){
+            llenarBDAlumnos();
+        }
+
+        //Añadir cursos a las cabeceras y alumnos a las listas hijas
+        ArrayList<Curso> listaCursos = bd.listarCursos();
+        if(listaCursos!=null){
+            for(int i=0;i<listaCursos.size();i++) {
+                listaCabeceras.add(listaCursos.get(i).getNombre());
+                if(listaAl !=null && listaAl.size()>0)
+                    listaAl.clear();
+                listaAl = bd.listarAlumnos(listaCursos.get(i).getID());
+                if(listaAl !=null) {
+                    grupoAl = new ArrayList<>();
+                    for (int j = 0; j < listaAl.size(); j++)
+                        grupoAl.add(listaAl.get(j).getNombre()+" "+listaAl.get(j).getApellido());
+                    listaHijos.put(listaCabeceras.get(i), grupoAl);
+                }
+                else
+                    Toast.makeText(getApplicationContext(), "Error al cargar alumnos del grupo", Toast.LENGTH_SHORT).show();
+            }
+        }
+        else
+            Toast.makeText(getApplicationContext(), "Error al cargar los cursos", Toast.LENGTH_SHORT).show();
+    }
+
+    public void llenarBDCursos(){
+        long[] indices = new long[4];
+
+        Curso c1 = new Curso(1,"Primero de DAW");
+        indices[0]= bd.insertarCurso(c1);
+        Curso c2 = new Curso(2,"Segundo de DAW");
+        indices[1]= bd.insertarCurso(c2);
+        Curso c3 = new Curso(3,"Primero de DAM");
+        indices[2]= bd.insertarCurso(c3);
+        Curso c4 = new Curso(4,"Segundo de DAM");
+        indices[3]= bd.insertarCurso(c4);
+
+        boolean correcto=true;
+        for(int i=0;i<indices.length;i++)
+            if(indices[i]==-1)
+                correcto=false;
+        if(correcto)
+            Toast.makeText(getApplicationContext(),"Cursos agregados correctamente",Toast.LENGTH_SHORT).show();
+        else
+            Toast.makeText(getApplicationContext(),"Error al agregar cursos",Toast.LENGTH_SHORT).show();
+    }
+
+    public void llenarBDAlumnos(){
+        long[] indices = new long[19];
+
+        Alumno a1 = new Alumno(1,"Pablo","Rodriguez Salas",1);
+        indices[0]=bd.insertarAlumno(a1);
+        Alumno a2 = new Alumno(2,"Nerea","Arbol Romero",1);
+        indices[1]=bd.insertarAlumno(a2);
+        Alumno a3 = new Alumno(3,"Manuel","Sanchez Bermudez",1);
+        indices[2]=bd.insertarAlumno(a3);
+        Alumno a4 = new Alumno(4,"Aitor","Mesa Lopez",1);
+        indices[3]=bd.insertarAlumno(a4);
+
+        Alumno a5 = new Alumno(5,"Ana","Lopera Portero",2);
+        indices[4]=bd.insertarAlumno(a5);
+        Alumno a6 = new Alumno(6,"Pedro","Calvente Puertas",2);
+        indices[5]= bd.insertarAlumno(a6);
+        Alumno a7 = new Alumno(7,"Alberto","Quesada Viruez",2);
+        indices[6]= bd.insertarAlumno(a7);
+        Alumno a8 = new Alumno(8,"Gonzalo","Garcia Grima",2);
+        indices[7]= bd.insertarAlumno(a8);
+        Alumno a9 = new Alumno(9,"Maria","Piedra Sampedro",2);
+        indices[8]= bd.insertarAlumno(a9);
+
+        Alumno a10 = new Alumno(10,"Mario","Arenas Sandoval",3);
+        indices[9]= bd.insertarAlumno(a10);
+        Alumno a11 = new Alumno(11,"Laura","Manzano Llorente",3);
+        indices[10]= bd.insertarAlumno(a11);
+        Alumno a12 = new Alumno(12,"Jose Antonio","Ortega Araque",3);
+        indices[11]= bd.insertarAlumno(a12);
+        Alumno a13 = new Alumno(13,"Sheila","Martinez Soga",3);
+        indices[12]= bd.insertarAlumno(a13);
+        Alumno a14 = new Alumno(14,"Salvador","Pinos Quesada",3);
+        indices[13]= bd.insertarAlumno(a14);
+
+        Alumno a15 = new Alumno(15,"Francisco","Pozo Arroyo",4);
+        indices[14]= bd.insertarAlumno(a15);
+        Alumno a16 = new Alumno(16,"Gloria","Sabinas Ortega",4);
+        indices[15]= bd.insertarAlumno(a16);
+        Alumno a17 = new Alumno(17,"Estefania","Fuentes Labrada",4);
+        indices[16]= bd.insertarAlumno(a17);
+        Alumno a18 = new Alumno(18,"Jose Enrique","Cuellar Arenas",4);
+        indices[17]= bd.insertarAlumno(a18);
+        Alumno a19 = new Alumno(19,"Guillermo","Luque Otero",4);
+        indices[18]= bd.insertarAlumno(a19);
+
+        boolean correcto=true;
+        for(int i=0;i<indices.length;i++)
+            if(indices[i]==-1)
+                correcto=false;
+        if(correcto)
+            Toast.makeText(getApplicationContext(),"Alumnos agregados correctamente",Toast.LENGTH_SHORT).show();
+        else
+            Toast.makeText(getApplicationContext(),"Error al agregar a los alumnos",Toast.LENGTH_SHORT).show();
     }
 
     @Override
@@ -60,16 +214,13 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+        //Opciones del menu
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
@@ -83,11 +234,11 @@ public class MainActivity extends AppCompatActivity
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
+        //Opciones del drawer
         int id = item.getItemId();
 
         if (id == R.id.nav_camera) {
-            // Handle the camera action
+
         } else if (id == R.id.nav_gallery) {
 
         } else if (id == R.id.nav_slideshow) {
